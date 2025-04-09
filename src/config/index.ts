396b59f0f55cfd9f780b992a2ee17adb4eece025
unknown <sayamalvi07@gmail.com>
2025-04-09 11:38:17 +0530

    add production run script

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,9 +4,7 @@ import path from 'path';
 config({
     path: path.join(__dirname, `../../.env.${process.env.NODE_ENV ?? 'dev'}`),
 });
-
 const { PORT, NODE_ENV, MONGO_URI, EMAIL, EMAIL_APP_PASS } = process.env;
-
 export const Config = {
     PORT,
     NODE_ENV,
